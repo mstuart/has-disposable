0119031ab3b52fb05f6fc98660c29e4499948e14
Mark Stuart <mstuart@users.noreply.github.com>
2026-08-09 21:08:35 -0700

    chore: enforce ultracite

--- index.test-d.ts
+++ index.test-d.ts
@@ -1,14 +1,14 @@
-import {expectType, expectError} from 'tsd';
-import hasDisposable, {hasAsyncDisposable} from './index.js';
+import { expectType } from "tsd";
+import hasDisposable, { hasAsyncDisposable } from "./index.js";
 
 const value: unknown = {};
 
 if (hasDisposable(value)) {
-	expectType<Disposable>(value);
+  expectType<Disposable>(value);
 }
 
 if (hasAsyncDisposable(value)) {
-	expectType<AsyncDisposable>(value);
+  expectType<AsyncDisposable>(value);
 }
 
 expectType<boolean>(hasDisposable({}));
